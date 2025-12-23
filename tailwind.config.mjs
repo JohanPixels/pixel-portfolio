465d3e4373b1b6aeef584251f427127dce98624c
JohanPixels <johanmunoz319@gmail.com>
2025-12-23 10:43:29 -0500

    Configuración inicial del proyecto, instalación de tailwind, creación base del componente de Hero, creación de funcionalidad de cambio de tema

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Congig} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
